d62e7eaabb92cef0d8d5aea29ff35b929b107d6e
Jared <TheWolfBadger@gmail.com>
2020-06-26 23:14:23 -0400

    Add files via upload

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'esx_ktackle - enables tackling for cops'
+description 'Badger_Tackle - enables tackling for cops'
 
 version '1.0.0'
 
