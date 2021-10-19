2515304c0e46c2eed62ac69d70c21473fbe9adbe
finlayrough <82729738+finlayrough@users.noreply.github.com>
2021-10-19 20:52:31 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'Badger_Tackle - enables tackling for cops'
+description 'Badger_Tackle - enables tackling'
 
 version '1.0.0'
 
